1a114f12623b6a0a091271a29880185a5f96cb6c
bnpdukim <bnpdukim@bnpinnovation.com>
2019-04-05 12:36:05 +0900

    변경

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -13,3 +13,4 @@ def export_script_and_view(model, os_path, contents_manager):
     subprocess.check_call(["jupyter", "nbconvert", "--to", "html", file_name, "--output", export_name + "_view"], cwd=dir_name)
 
 c.FileContentsManager.post_save_hook = export_script_and_view
+c.NotebookApp.disable_check_xsrf = True
